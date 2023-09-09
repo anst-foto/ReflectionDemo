389082fa4fa8feffa87cd95161fbf2256b1d2c90
Starinin Andrey <starininandrey@gmail.com>
2023-09-09 10:31:45 +0300

    Init

---
+++ ReflectionDemo/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ReflectionDemo;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int age;
+
+    public Person()
+    {
+    }
+
+    public Person(int age, string name)
+    {
+        this.age = age;
+        Name = name;
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using ReflectionDemo;
+
+var reflection = typeof(Person);
+var constructors = reflection.GetConstructors();
+foreach (var c in constructors)
+{
+    Console.Write($"{c.Name} : ");
+    foreach (var p in c.GetParameters())
+    {
+        Console.WriteLine($"\t{p.Position} - {p.Name}");
+    }
+    Console.WriteLine();
+}
